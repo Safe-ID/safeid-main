273a2bd9511b7b28b71a691cf176e79bcd3a4c3e
ucasvieira <lucasvieirasouza5@gmail.com>
2026-06-01 00:17:44 -0300

    Template

---
+++ client/src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Safe_ID from './Safe_ID.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Safe_ID />
+  </StrictMode>,
+)
